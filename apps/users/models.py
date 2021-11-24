0175ba3b6c7950585334ffefeb40f71df310f8af
lautaroalejo02 <lautitomasalejo@gmail.com>
2021-11-24 15:03:21 -0300

    Required removed from models

--- apps/users/models.py
+++ apps/users/models.py
@@ -3,9 +3,9 @@ from datetime import date
 
 class User(db.Document):
     userId = db.SequenceField(primary_key=True)
-    name = db.StringField(required = True)
-    email = db.EmailField(required = True)
-    password = db.StringField(required = True)
+    name = db.StringField()
+    email = db.EmailField()
+    password = db.StringField()
     profilePicture = db.ImageField(default = "noPic", required = False)
     registerDate = db.DateTimeField(required = True, default = date.today())
     chatRoomId = db.IntField(required = True, default = 0)
