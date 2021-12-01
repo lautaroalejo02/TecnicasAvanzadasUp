464eb25510956751505a6a3f2ffb65f7314d4f96
lautaroalejo02 <lautitomasalejo@gmail.com>
2021-12-01 18:37:55 -0300

    dff

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, jsonify, Blueprint, session, current_app, render_template
+from flask import Flask, request, jsonify, Blueprint, session, current_app,render_template
 import json
 import hashlib
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_mongoengine import MongoEngine
-from apps.users.users import usersApp ,register_user
+from apps.users.users import usersApp ,User #register_user
 from apps.chats.chats import chatsApp
 from flask_jwt_extended import JWTManager, create_access_token
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO, send, emit
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+
 app.config['MONGODB_SETTINGS'] = {
     'db': 'ChatAppTecnicasAvanzadas',
     'host': 'localhost',
@@ -22,15 +23,37 @@ app.register_blueprint(usersApp, url_prefix='/api/users')
 app.register_blueprint(chatsApp, url_prefix='/api/chats')
 app.config["JWT_SECRET_KEY"] = "secret"  # Change this "super secret" with something else!
 jwt = JWTManager(app)
-
-@app.route('/')
+socketio = SocketIO(app, cors_allowed_origins="*")
+CORS(app)
+userss = []
+@app.route('/s')
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
+
 
 @socketio.on('message')
-def handleMessage(msg):
+def handleMessage(msg, methods=['GET', 'POST']):
     print('Message: ' + msg)
-    send(msg, broadcast = True)
+    socketio.emit('message', msg)
+
+@socketio.on('connect2')
+def on_connect():
+    msg="asdasd"
+    print('Client connected')
+    send(msg)
+
+
+@socketio.on('sendMessage')
+def handleMessage2(messageInfo):
+    print(messageInfo)
+    user = User.objects(userId=messageInfo['uID']).first()
+    kala = {
+        "_id": messageInfo['uID'], "chatRoom": messageInfo['crID'], "messageText": messageInfo['txtMsg'],
+        "userName": user.name}
+    userss.append(kala)
+    print(userss)
+    socketio.emit((userss))
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
